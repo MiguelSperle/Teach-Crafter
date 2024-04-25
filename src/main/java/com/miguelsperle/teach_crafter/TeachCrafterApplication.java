a6275c8f599dd1ce30ed256f4b833e5569666a08
MiguelSperle <miguelsperledeveloper@gmail.com>
2024-04-25 17:09:42 -0300

    feat: create tables on database

---
+++ src/main/java/com/miguelsperle/teach_crafter/TeachCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.miguelsperle.teach_crafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachCrafterApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V4__create-indexes.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX users_email_username_key ON users(email, username);
